97f2e1e8fedd6f99f8d2db698c618095a23b93d5
drdlee <danielrudi.lee@gmail.com>
2018-01-17 04:34:49 +0800

    adding dashboard, only showing user own post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,25 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create New Post</a>
                     <h3>Your Blog Posts</h3>
+                    <table class="table table-striped">
+                      <tr>
+                        <th>Title</th>
+                        <th></th>
+                        <th></th>
+                      </tr>
+                      @foreach($posts as $post)
+                        <tr>
+                          <td>{{$post->title}}</td>
+                          <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">edit</a></td>
+                          <td>
+                            {!!Form::open(['action'=> ['PostsController@destroy', $post->id], 'method'=> 'POST', 'class'=> 'pull-right'])!!}
+                              {{form::hidden('_method', 'DELETE')}}
+                              {{form::submit('Delete', ['class'=>'btn btn-danger'])}}
+                            {!!Form::close()!!}
+                          </td>
+                        </tr>
+                      @endforeach
+                    </table>
                 </div>
             </div>
         </div>
