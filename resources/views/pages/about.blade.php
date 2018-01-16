9293418093ce48f6f66deb17cfd81dad20c61c84
drdlee <danielrudi.lee@gmail.com>
2018-01-16 20:40:28 +0800

    first really late commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+  <h1>About</h1>
+  <p>Bacon ipsum dolor amet boudin andouille hamburger, brisket corned beef pork cupim jowl doner meatball leberkas. Meatloaf sirloin salami, pastrami ball tip turkey rump boudin. Buffalo spare ribs chuck landjaeger pork belly shoulder doner ribeye ham porchetta sausage. Leberkas pork loin andouille chuck landjaeger meatball strip steak frankfurter turducken jerky rump filet mignon. Kevin short loin corned beef picanha t-bone.</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/user/{id}/{name}', function ($id, $name){
+  return "This is the User page, with userID: {$id} and name: {$name}";
+});
+*/
+
+Route::get('/', 'PagesController@Index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
