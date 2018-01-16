c3525bd8f47080ee8032461dd99fb9e34ee79904
drdlee <danielrudi.lee@gmail.com>
2018-01-16 21:47:09 +0800

    looping in blade, add navbar, add jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
-  <h1>{{$title}}</h1>
-  <p>Bacon ipsum dolor amet boudin andouille hamburger, brisket corned beef pork cupim jowl doner meatball leberkas. Meatloaf sirloin salami, pastrami ball tip turkey rump boudin. Buffalo spare ribs chuck landjaeger pork belly shoulder doner ribeye ham porchetta sausage. Leberkas pork loin andouille chuck landjaeger meatball strip steak frankfurter turducken jerky rump filet mignon. Kevin short loin corned beef picanha t-bone.</p>
+  <div class="jumbotron text-center">
+    <h1>Hello, world!</h1>
+    <p>Bacon ipsum dolor amet boudin andouille hamburger, brisket corned beef pork cupim jowl doner meatball leberkas.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+  </div>
 @endsection
