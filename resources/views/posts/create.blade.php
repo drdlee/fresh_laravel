c90ab58f7a4435ea3039ec320da35a1682e96cf9
drdlee <danielrudi.lee@gmail.com>
2018-01-17 02:32:15 +0800

    adding laravelCollective (laravel form) and using it in create.blade.php

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+  <h3>Add New Post</h3>
+  {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST']) !!}
+    <div class="form-group">
+      {{form::label('title', 'Title')}}
+      {{form::text('title', '', ['class'=> 'form-control', 'placeholder'=> 'Title'])}}
+    </div>
+    <div class="form-group">
+      {{form::label('body', 'Content')}}
+      {{form::textarea('body', '', ['class'=> 'form-control', 'placeholder'=> 'Content'])}}
+    </div>
+    {{form::submit('Submit', ['class'=>'btn btn-primary'])}}
+  {!! Form::close() !!}
+@endsection
