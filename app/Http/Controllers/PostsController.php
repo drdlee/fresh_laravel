d9fb92703fccef5a0d23b3e1f2ca76b7a262a0d8
drdlee <danielrudi.lee@gmail.com>
2018-01-17 02:48:16 +0800

    submitting to database, success/error message, blank form validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -42,7 +42,16 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+          'title' => 'required',
+          'body' => 'required'
+        ]);
+
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        return redirect('/posts')->with('success', 'Post Created');
     }
 
     /**
